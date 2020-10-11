bfff6c09cc23850d66974993d0ca2dc27be54f42
Nicolas França <nicolasfranca9@gmail.com>
2020-10-11 02:11:21 -0300

    Começo das dúvidas...

---
+++ 11-09-2020/uri/1340/1340.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+	// ...
+	// Então temos que ficar lendo e relendo esse 
+	// problema, e entender e implementar em c++.
+	//
+	// :)
+	return 0;
+}
